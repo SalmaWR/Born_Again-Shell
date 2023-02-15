a777ae7bc283884064daedf06ac99bc3278d0978
SalmaWR <Salma.wagdy@hotmail.com>
2023-02-15 21:14:21 +0200

    Cool

--- fork.c
+++ fork.c
@@ -8,6 +8,19 @@ int main (int agrc, char *argv[])
 	/*to implement parallel processing*/
 	int id = fork();
 	printf("hello world from id:%d\n", id);
+	if (id == 0)
+	{
+		printf("this is the child process\n");
+	
+	}else{
+		printf("this is the main process\n");
+	}
 	return 0;
 }
 
+/*
+* what fork() function do is forking my excution line
+* we can say that a child process gets born 
+* everthing before fork function is excuted once
+* the ID of the child process is gonna be zero 
+*/
---
+++ wait.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<time.h>
+
+int main( int argc, char* argv[])
+{
+
+int id = fork();
+int n;
+
+//child process
+if (id == 0)
+{
+n = 1;
+}
+//main process
+else
+{
+n = 6;
+}
+
+if(id != 0);
+{
+wait();
+}
+
+for (int i = n; i < n + 5; i++)
+{
+printf("%d", i); 
+fflush(stdout);
+}
+printf("\n");
+return 0;
+
+}
